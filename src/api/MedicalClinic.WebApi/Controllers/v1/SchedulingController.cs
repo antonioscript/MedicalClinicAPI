7b1460e1f0843c9be9418468de2c05119ae93c18
Antônio Rocha <antoniojunior159@gmail.com>
2024-01-27 16:05:34 -0300

    Created Specific Methd for the all procedures Patient

---
+++ src/api/MedicalClinic.WebApi/Controllers/v1/SchedulingController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using MedicalClinic.Application.DTOs.Scheduling;
+using MedicalClinic.Application.Features.Schedulings.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalClinic.WebApi.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchedulingController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SchedulingController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+       
+        [HttpGet("Filter")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAllFilter([FromQuery] SchedulingRequestFilter filter)
+        {
+            var results = await _mediator.Send(new GetAllSchedulingByFilterQuery(filter));
+            return Ok(results);
+        }
+
+
+    }
+}
+
+
+
+
+
+
